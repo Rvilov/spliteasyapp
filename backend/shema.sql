71885862b87ddcb3984ace9345b6549906e2e5a6
rvilov <ricardovilov@gmail.com>
2026-05-27 02:24:45 +0200

    Cambiado a ProtectedRoute

--- backend/shema.sql
+++ backend/shema.sql
@@ -1,9 +1,4 @@
-CREATE TABLE users(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(50),
-  email VARCHAR(50),
-  password VARCHAR(100)
-);
+CREATE TABLE users(   id SERIAL PRIMARY KEY,  name VARCHAR(50),  email VARCHAR(50),  password VARCHAR(100));
 
 CREATE TABLE groups(
   id SERIAL PRIMARY KEY,
